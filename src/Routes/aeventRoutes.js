6d6cb945cee30c6c385d0e9d74fb8c8e13aca669
RobertCardoso <robert.rcardoso@hotmail.com>
2022-06-21 21:01:13 -0500

    done

--- src/Routes/aeventRoutes.js
+++ src/Routes/aeventRoutes.js
@@ -3,7 +3,7 @@ let router = new express.Router();
 let controller = require("../Controllers/aeventController");
 let auth = require("../middleware/auth");
 
-router.get("/events", auth.verifyJWT, controller.listEvents);
+router.get("/events", controller.listEvents);
 
 router.get("/events/:id", auth.verifyJWT, controller.eventDetails)
 
